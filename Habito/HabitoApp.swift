4121a12d35b7e4680ba4561f099dfc2c9b788cbe
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-27 10:10:12 -0400

    Initial Commit

---
+++ Habito/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by Gabriel Marquez on 2022-06-27.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
